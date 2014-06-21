8ab7f0727db0b626d3dad21684cff3b984e1bf8a
pisketti <tero.kadenius@gmail.com>
2014-06-22 02:37:38 +0300

    Adds url and description to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject gae-app-demo "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Clojure App Engine tutorial source code"
+  :url "http://flowa.fi"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.1.6"]
                  [hiccup "1.0.5"]
